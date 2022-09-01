25c9eb415d0db0756a7e5ec0f543bd293f2357fa
Michael Moran (MacBook) <haelmic3@gmail.com>
2022-09-01 00:21:04 -0400

    Comment on color scheme bug

--- hello/hello.py
+++ hello/hello.py
@@ -2,6 +2,8 @@ import tkinter as tk
 
 root = tk.Tk()
 
+#FIXME uses system background but not system text color so black on black is possible
+
 hello = tk.Label(root, text="Hello, World!").pack()
 
 root.mainloop()
